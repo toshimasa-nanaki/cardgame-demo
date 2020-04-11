cc994b68190f9437b8263c9063d5743404b7de64
Glitch (kentei-daihugou) <none>
2020-04-11 16:51:33 +0000

    😚⛹ Checkpoint ./package.json:14189999/145 ./index.js:14189999/448 ./index.html:14189999/1443

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var http = require('http');
-http.createServer(function (request, response) {
-	  response.writeHead(200, {'Content-Type': 'text/plain'});
-	  response.end('Hello World2\n');
-}).listen(3000);
+// var http = require('http');
+// http.createServer(function (request, response) {
+// 	  response.writeHead(200, {'Content-Type': 'text/plain'});
+// 	  response.end('Hello World2\n');
+// }).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3000');
+// console.log('Server running at http://127.0.0.1:3000');
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
