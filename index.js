4012bcb4781264b1efbb27928d336908c13bda5c
Glitch (kentei-daihugou) <none>
2020-04-12 14:24:28 +0000

    🚤📅 Checkpoint ./index.js:14189999/2749

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// var http = require('http');
-// http.createServer(function (request, response) {
-// 	  response.writeHead(200, {'Content-Type': 'text/plain'});
-// 	  response.end('Hello World2\n');
-// }).listen(3000);
-
-// console.log('Server running at http://127.0.0.1:3000');
+const TRUMPDATA = {
+    card: [
+        { type: 'clover', count: 13 },
+        { type: 'spade', count: 13 },
+        { type: 'heart', count: 13 },
+        { type: 'diamond', count: 13 }
+    ],
+    joker: 1
+}
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var store = {};
+const ORIGINALCARDDATA = trump_init(TRUMPDATA);
+const shuffleCards = sort_at_random(ORIGINALCARDDATA);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -28,13 +32,12 @@ io.on('connection', function(socket){
     socket.join(msg.roomid);
   });
   socket.on('join', function(msg) {
-    // const usrobj = {
-    //   'room': msg.roomid,
-    //   'name': msg.name
-    // };
-    // console.log(msg.roomid);
-    // store[msg.id] = usrobj;
-    socket.join(msg.roomid);
+    const count = typeof store[msg.id].count === "undefined" ? 4 : store[msg.id].count;
+    if(socket.nsp.adapter.rooms[msg.id].length >= count){
+      io.to(socket.id).emit('update', "もう部屋がいっぱいです");
+    }else{
+      socket.join(msg.roomid);
+    }
   });
   socket.on('update', function(msg){
     //io.emit('chat message', socket.client.conn.server.clientsCount);
@@ -45,22 +48,41 @@ io.on('connection', function(socket){
     }else{
       io.to(store[msg.id].room).emit('update', "今の部屋の人数:  " + socket.nsp.adapter.rooms[msg.id].length);
     }
-    // while(true){
-    //   io.to(store[msg.id].room).emit('update', "今の部屋の人数:  " + socket.nsp.adapter.rooms[msg.id].length);
-    //   if(socket.nsp.adapter.rooms[msg.id].length == count){
-    //     io.to(store[msg.id].room).emit('gathered', count + "人が集まりました！！！");
-    //     break;
-    //   }
-    //   if(retryCount >= 10){
-    //     io.to(store[msg.id].room).emit('update', "集まれませんでした。。");
-    //     return;
-    //   }
-    //   //sleep(5000);
-    //   retryCount++;
-    // }
     
   });
 });
+
+function trump_init(trumpData) {
+    var cards = [];
+    for (var i = 0; i < trumpData['card'].length; i++) {
+        var thistype = trumpData['card'][i];
+        for (var j = 0; j < thistype['count']; j++) {
+            cards.push({
+                type: thistype['type'],
+                number: j + 1
+            });
+        }
+    }
+    for (var i = 0; i < trumpData['joker']; i++) {
+        cards.push({
+            type: 'joker',
+            number: i + 1
+        });
+    }
+    return cards;
+}
+
+function sort_at_random(arrayData) {
+    var arr = arrayData.concat();
+    var arrLength = arr.length;
+    var randomArr = [];
+    for(var i = 0; i < arrLength; i++) {
+        var randomTarget = Math.floor(Math.random() * arr.length);
+        randomArr[i] = arr[randomTarget];
+        arr.splice(randomTarget, 1);
+    }
+    return randomArr;
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
