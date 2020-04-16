9841aa86521cba5f10992710b32640e2d83e83ce
Glitch (kentei-daihugou) <none>
2020-04-16 03:03:25 +0000

    🌴👨‍👨‍👦 Checkpoint ./public/js/app.js:14189999/1132 ./index.html:14189999/1930

--- public/js/app.js
+++ public/js/app.js
@@ -79,41 +79,23 @@ $(function() {
     console.log("CreatedRoom");
     const div = $('<div class="form-check"></div>');
     div.append($('<input type="radio" />').attr({
+          class: "form-check-input",
           name: "roomRadios",
           value: "option1",
           id: "room_"+roomList.roomId
           //value: element.type + "_" + element.number
         }));
     div.append($('<label class="form-check-label" for="' + "room_" + roomList.roomId + '">' + roomList.roomDispName + "</label>"));
-    $("#selectRoomList").append(div);// window.scrollTo(0, document.body.scrollHeight);
+    $("#selectRoomList").append(div);
   });
-  // <input
-  //                       class="form-check-input"
-  //                       type="radio"
-  //                       name="exampleRadios"
-  //                       id="exampleRadios1"
-  //                       value="option1"
-  //                       checked
-  //                     />
-  //                     <label class="form-check-label" for="exampleRadios1">
-  //                       Default radio
-  //                     </label>
-  // var check = $(
-  //       '<label id="' +
-  //         element.type +
-  //         element.number +
-  //         '">' +
-  //         DISPLAY_DIC[element.type + element.number] +
-  //         "</label>"
-  //     ).prepend(
-  //       $('<input type="checkbox" />').attr({
-  //         name: "cards",
-  //         value: element.type + "_" + element.number
-  //       })
-  //     );
-  //     $("#cardList").append(check);
+
   socket.on("showRoomList", function(roomList) {
-    console.log("CreatedRoom");
+    console.log("ShowRoom");
+    Object.keys(roomList).for
+    for(let i=0; i < roomList.length; i++){
+      console.log(roomList[i]);
+    }
+    
     // $("#connectStatus").append($("<li>").text(msg));
     // window.scrollTo(0, document.body.scrollHeight);
   });
