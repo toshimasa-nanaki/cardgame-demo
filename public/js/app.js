2d74b04637a75efff4c75f50e970f655722179b4
Glitch (kentei-daihugou) <none>
2020-04-19 08:05:32 +0000

    ⏱🏐 Checkpoint ./public/js/app.js:14189999/226

--- public/js/app.js
+++ public/js/app.js
@@ -131,13 +131,20 @@ $(function() {
 	});
   // 画像がクリックされた時の処理です。
   $('img.handCardImage').click(function() {
-    var $imageList = $('.image_list');
+    if (!$(this).is('.checked')) {
+      // チェックが入っていない画像をクリックした場合、チェックを入れます。
+      $(this).addClass('checked');
+    } else {
+      // チェックが入っている画像をクリックした場合、チェックを外します。
+      $(this).removeClass('checked')
+    }
+//     var $imageList = $('.image_list');
 
-  // 現在の選択を解除します。
-  //$imageList.find('img.thumbnail.checked').removeClass('checked');
+//   // 現在の選択を解除します。
+//   //$imageList.find('img.thumbnail.checked').removeClass('checked');
 
-    // チェックを入れた状態にします。
-    $(this).addClass('checked');
+//     // チェックを入れた状態にします。
+//     $(this).addClass('checked');
   });
   $('.image_box .disabled_checkbox').click(function() {
     return false;
