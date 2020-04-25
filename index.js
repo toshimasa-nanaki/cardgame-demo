2fb227e40e2862ac671fa856dd4448a917b20bb4
Glitch (kentei-daihugou) <none>
2020-04-25 02:26:10 +0000

    🌠🍭 Checkpoint ./notifyUtil.js:14189999/1568 ./index.js:14189999/108

--- index.js
+++ index.js
@@ -984,42 +984,42 @@ function notifyGiveCard(roomId, memberCount) {
 //   });
 // }
 
-function notifyGameReady(roomId) {
-  storeData.persistentData[roomId].giveCardCount = 0;
-  const orders = storeData.persistentData[roomId]["order"];
-  const users = storeData.persistentData[roomId]["users"];
-  io.to(orders[0]).emit("gameReady", {
-    gameNum: storeData.persistentData[roomId].gameNum,
-    card: users[orders[0]].card,
-    yourTurn: true,
-    playerName: users[orders[0]].dispName
-  });
-  LOGGER.debug(
-    "gameReadyのレスポンス(一番目)： " +
-      JSON.stringify({
-        gameNum: storeData.persistentData[roomId].gameNum,
-        card: users[orders[0]].card,
-        yourTurn: true,
-        playerName: users[orders[0]].dispName
-      })
-  );
-  for (let i = 1; i < storeData.persistentData[roomId]["order"].length; i++) {
-    io.to(orders[i]).emit("gameReady", {
-      gameNum: storeData.persistentData[roomId].gameNum,
-      card: users[orders[i]].card,
-      yourTurn: false,
-      playerName: users[orders[0]].dispName
-    });
-    LOGGER.debug(
-      "gameReadyのレスポンス(二番目以降)： " +
-        JSON.stringify({
-          card: users[orders[i]].card,
-          yourTurn: false,
-          playerName: users[orders[0]].dispName
-        })
-    );
-  }
-}
+// function notifyGameReady(roomId) {
+//   storeData.persistentData[roomId].giveCardCount = 0;
+//   const orders = storeData.persistentData[roomId]["order"];
+//   const users = storeData.persistentData[roomId]["users"];
+//   io.to(orders[0]).emit("gameReady", {
+//     gameNum: storeData.persistentData[roomId].gameNum,
+//     card: users[orders[0]].card,
+//     yourTurn: true,
+//     playerName: users[orders[0]].dispName
+//   });
+//   LOGGER.debug(
+//     "gameReadyのレスポンス(一番目)： " +
+//       JSON.stringify({
+//         gameNum: storeData.persistentData[roomId].gameNum,
+//         card: users[orders[0]].card,
+//         yourTurn: true,
+//         playerName: users[orders[0]].dispName
+//       })
+//   );
+//   for (let i = 1; i < storeData.persistentData[roomId]["order"].length; i++) {
+//     io.to(orders[i]).emit("gameReady", {
+//       gameNum: storeData.persistentData[roomId].gameNum,
+//       card: users[orders[i]].card,
+//       yourTurn: false,
+//       playerName: users[orders[0]].dispName
+//     });
+//     LOGGER.debug(
+//       "gameReadyのレスポンス(二番目以降)： " +
+//         JSON.stringify({
+//           card: users[orders[i]].card,
+//           yourTurn: false,
+//           playerName: users[orders[0]].dispName
+//         })
+//     );
+//   }
+// }
 
 function removeCard(sc, userId, roomId) {
   //let arr = [];
--- notifyUtil.js
+++ notifyUtil.js
@@ -1,5 +1,48 @@
+"use strict";
+
+const commonUtil = require("./commonUtil.js");
+const gameUtil = require("./gameUtil.js");
+const storeData = require("./storeData.js");
+const loggerUtil = require("./loggerUtil.js");
+const LOGGER = loggerUtil.logger;
+const index = require("./index.js");
+const io = index.io;
+
 module.exports.notifyGameReady = roomId => {
-  
+  storeData.persistentData[roomId].giveCardCount = 0;
+  const orders = storeData.persistentData[roomId]["order"];
+  const users = storeData.persistentData[roomId]["users"];
+  io.to(orders[0]).emit("gameReady", {
+    gameNum: storeData.persistentData[roomId].gameNum,
+    card: users[orders[0]].card,
+    yourTurn: true,
+    playerName: users[orders[0]].dispName
+  });
+  LOGGER.debug(
+    "gameReadyのレスポンス(一番目)： " +
+      JSON.stringify({
+        gameNum: storeData.persistentData[roomId].gameNum,
+        card: users[orders[0]].card,
+        yourTurn: true,
+        playerName: users[orders[0]].dispName
+      })
+  );
+  for (let i = 1; i < storeData.persistentData[roomId]["order"].length; i++) {
+    io.to(orders[i]).emit("gameReady", {
+      gameNum: storeData.persistentData[roomId].gameNum,
+      card: users[orders[i]].card,
+      yourTurn: false,
+      playerName: users[orders[0]].dispName
+    });
+    LOGGER.debug(
+      "gameReadyのレスポンス(二番目以降)： " +
+        JSON.stringify({
+          card: users[orders[i]].card,
+          yourTurn: false,
+          playerName: users[orders[0]].dispName
+        })
+    );
+  }
 }
 
 module.exports.notifyGiveCard = (roomId,playerNum) => {
