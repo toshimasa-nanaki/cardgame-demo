e7e5de946b4e0b74cdbbdd5c9468f1e982b522e7
Glitch (kentei-daihugou) <none>
2020-04-26 04:24:19 +0000

    🌒🚙 Checkpoint ./gameUtil.js:14189999/61

--- gameUtil.js
+++ gameUtil.js
@@ -86,7 +86,7 @@ const handOutCards = (count, roomId) => {
   LOGGER.debug("perNum:" + perNum + " remainder:" + remainder);
   //ブラインドカードの確認をする。もしジョーカーが含まれている場合は切りなおす。
   while(shuffleCards.slice(TRUMP_TEMP.total - 2, TRUMP_TEMP.total).some(ele => ~ele.type.indexOf("joker"))){
-    LOGGER.debug("ブラインドカードにジョーカーが含まれるためシャッフルしなおす");
+    LOGGER.debug("ブラインドカードにジョーカーが含まれるためシャッフルしなおす" + shuffleCards.slice(TRUMP_TEMP.total - 2, TRUMP_TEMP.total));
     shuffleCards = commonUtil.sortArrayRandomly(ORIGINALCARDDATA);
   }
   let pos = 0;
