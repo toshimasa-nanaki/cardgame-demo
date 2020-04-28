eced0634970684e87370dae51511edf947de794a
Glitch (kentei-daihugou) <none>
2020-04-28 13:32:06 +0000

    🐂🌠 Checkpoint ./storeData.js:14189999/706

--- storeData.js
+++ storeData.js
@@ -38,6 +38,21 @@ module.exports.fieldClear = (roomId) => {
   module.exports.persistentData[roomId].shibari = false;
 }
 
-module.exports.sortCard = (isASC = true) => {
-  
+module.exports.sortCard = (roomId, userId, isASC = true) => {
+  module.exports.persistentData[roomId]['users'][userId].card = module.exports.persistentData[roomId]['users'][userId].card.sort((a,b)=>{
+    if(isASC){
+      //番号の昇順
+    }else{
+      //
+    }
+  });
+  storeData.persistentData[msg.id]["users"][storeData.persistentData[msg.id]["order"][yourOrder]].giveCard = [];
+      //こちらのカードを相手に渡す。
+      storeData.persistentData[msg.id]["users"][storeData.persistentData[msg.id]["order"][yourOrder]].card.push(
+        msg.cards[0]
+      );
+      let validateCards = msg.cards.sort(function(a, b) {
+      if (a.number < b.number) return -1;
+      if (a.number > b.number) return 1;
+      return 0;
 }
