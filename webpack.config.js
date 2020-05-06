9218e01fd6bd3945e30df11a47790b87c835ea34
Glitch (kentei-daihugou) <none>
2020-05-06 08:44:29 +0000

    🐃🌴 Checkpoint ./webpack.config.js:14189999/4230 ./package.json:14189999/26

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = [
+  //クライアント側(javascript)
+  {
+    mode: "development",
+    entry: {
+      client: "./src/client/index.js",
+    },
+    output: {
+      filename: "[name].js",
+      path: `${__dirname}/public/js`,
+    },
+  },
+  //サーバ側
+  {
+    mode: "development",
+    entry: {
+      server: "./index.js",
+    }
+  }
+];
