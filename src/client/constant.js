4cded8c77a33cb73c46d50d2dcbee947032d6661
Glitch (kentei-daihugou) <none>
2020-05-14 05:05:10 +0000

    🌝👌 Checkpoint ./src/client/index.js:14189999/59930 ./src/client/constant.js:14189999/667

---
+++ src/client/constant.js
@@ -0,0 +1,25 @@
+module.exports.SUITES_DIC = {
+    spade: "♠",
+    club: "♣",
+    diamond: "♦",
+    heart: "♥"
+  };
+
+module.exports.RANKING_DIC = {
+    daihugou: "大富豪",
+    hugou: "富豪",
+    heimin: "平民",
+    hinmin: "貧民",
+    daihinmin: "大貧民"
+  };
+
+module.exports.ERROR_DIC = {
+    diffNumOfCards: "カードの枚数は合わせてください。",
+    diffSuitCards: "スートしばりに合ったカードを出してください。",
+    loseCards: "場のカードより強いものを出してください。",
+    handError: "役ができていません。",
+    fieldNothing: "フィールドにカードがない状態でパスはできません。",
+    goOutRoom: "プレイヤーの数が減ったためゲームを中断します。\nゲームをやめる場合はページを再読み込みしてください。",
+    roomFull: "この部屋は既に定員に達しています。",
+    roomNothing: "この部屋は既に削除されています。別の部屋に参加するか、新規で部屋を作成してください。"
+  };
