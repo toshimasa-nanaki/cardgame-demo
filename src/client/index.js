06ee1c0a1198e2444d1ea07ec7406dfb609b00c8
Glitch (kentei-daihugou) <none>
2020-05-23 01:21:00 +0000

    🌂🐘 Checkpoint ./src/client/index.js:14189999/672

--- src/client/index.js
+++ src/client/index.js
@@ -29,21 +29,41 @@ $(function() {
   mypreload();
 
   const testTable = () => {
+    // 表の作成開始
+    var rows=[];
+    var table = document.createElement("table");
+
+    // 表に2次元配列の要素を格納
+    for(i = 0; i < data.length; i++){
+        rows.push(table.insertRow(-1));  // 行の追加
+        for(j = 0; j < data[0].length; j++){
+            cell=rows[i].insertCell(-1);
+            cell.appendChild(document.createTextNode(data[i][j]));
+            // 背景色の設定
+            if(i==0){
+                cell.style.backgroundColor = "#bbb"; // ヘッダ行
+            }else{
+                cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
+            }
+        }
+    }
+    // 指定したdiv要素に表を加える
+    document.getElementById(tableId).appendChild(table);
     //document.getElementById("tbo").innerHTML = "";
-    let table = document.getElementById("thisRankTable");
-    let newRow = table.insertRow(-1);
+//     let table = document.getElementById("thisRankTable");
+//     let newRow = table.insertRow(-1);
 
-    let newCell = newRow.insertCell();
-    let newText = document.createTextNode("大富豪");
-    newCell.appendChild(newText);
+//     let newCell = newRow.insertCell();
+//     let newText = document.createTextNode("大富豪");
+//     newCell.appendChild(newText);
 
-    newCell = newRow.insertCell();
-    newText = document.createTextNode("賢帝");
-    newCell.appendChild(newText);
+//     newCell = newRow.insertCell();
+//     newText = document.createTextNode("賢帝");
+//     newCell.appendChild(newText);
     
-    newCell = newRow.insertCell();
-    newText = document.createTextNode("都落ち");
-    newCell.appendChild(newText);
+//     newCell = newRow.insertCell();
+//     newText = document.createTextNode("都落ち");
+//     newCell.appendChild(newText);
   };
   testTable();
 
